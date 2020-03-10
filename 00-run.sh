a1d9613820b12595b5f43244293a14d285e55302
Len Budney <Len.Budney@grantstreet.com>
2020-03-10 10:48:59 -0400

    Fix for DNS problems downloading source files in docker build.

--- 00-run.sh
+++ 00-run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 # Download source files
 
+cat /etc/resolv.conf > /tmp/resolv.conf.bak
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+cleanup() {
+    cat /tmp/resolv.conf.bak > /etc/resolv.conf
+}
+trap cleanup EXIT
+
 for URL in $(cat sources.txt); do
     ( cd files; curl -O ${URL}; )
 done
