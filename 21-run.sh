8279197eee813b8fd951692a7e621a54ee05dbdc
Len Budney <Len.Budney@grantstreet.com>
2020-03-10 11:02:51 -0400

    Fixed bungled sed expressions when I removed usernames/passwords.

--- 21-run.sh
+++ 21-run.sh
@@ -12,9 +12,9 @@ install -m 440 -o root -g root files/revaliases "${ROOTFS_DIR}/etc/ssmtp/"
 # SSMTP configs
 for file in ssmtp.conf revaliases; do
     install -m 440 -o root -g root files/${file} "${ROOTFS_DIR}/etc/ssmtp/"
-    sed -i -e "s/SSMTP_USER/${SSMTP_USER}" "${ROOTFS_DIR}/etc/ssmtp/${file}"
-    sed -i -e "s/SSMTP_PASS/${SSMTP_PASS}" "${ROOTFS_DIR}/etc/ssmtp/${file}"
-    sed -i -e "s/FIRST_USER_NAME/${FIRST_USER_NAME}" "${ROOTFS_DIR}/etc/ssmtp/${file}"
-    sed -i -e "s/SSMTP_ROOT_ALIAS/${SSMTP_ROOT_ALIAS}" "${ROOTFS_DIR}/etc/ssmtp/${file}"
+    sed -i -e "s/SSMTP_USER/${SSMTP_USER}/" "${ROOTFS_DIR}/etc/ssmtp/${file}"
+    sed -i -e "s/SSMTP_PASS/${SSMTP_PASS}/" "${ROOTFS_DIR}/etc/ssmtp/${file}"
+    sed -i -e "s/FIRST_USER_NAME/${FIRST_USER_NAME}/" "${ROOTFS_DIR}/etc/ssmtp/${file}"
+    sed -i -e "s/SSMTP_ROOT_ALIAS/${SSMTP_ROOT_ALIAS}/" "${ROOTFS_DIR}/etc/ssmtp/${file}"
 done
 
