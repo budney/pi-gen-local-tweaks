3fa2b315fb9abd3b78724a63d92f268e368eabdc
Len Budney <Len.Budney@grantstreet.com>
2020-03-15 14:39:00 -0400

    Changed network gateway into a config parameter.

--- 92-run.sh
+++ 92-run.sh
@@ -3,6 +3,7 @@
 # Network config
 install -m 644 -o root -g root files/eth0 "${ROOTFS_DIR}/etc/network/interfaces.d/"
 sed -i -e "s}HOST_IP}${HOST_IP}}" "${ROOTFS_DIR}/etc/network/interfaces.d/eth0"
+sed -i -e "s}NET_GATEWAY}${NET_GATEWAY}}" "${ROOTFS_DIR}/etc/network/interfaces.d/eth0"
 
 # DNS Config: uncomment the name_servers line in resolvconf.conf
 sed -i -e '/name_servers=/s/^#//' "${ROOTFS_DIR}/etc/resolvconf.conf"
