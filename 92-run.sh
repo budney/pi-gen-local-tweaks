a61d1383b9b87373a41ef3f08b1970c971bf9bd2
Len Budney <Len.Budney@grantstreet.com>
2020-03-15 14:40:05 -0400

    Cleaned up the /etc/hosts line created during image setup.

--- 92-run.sh
+++ 92-run.sh
@@ -8,3 +8,5 @@ sed -i -e "s}NET_GATEWAY}${NET_GATEWAY}}" "${ROOTFS_DIR}/etc/network/interfaces.
 # DNS Config: uncomment the name_servers line in resolvconf.conf
 sed -i -e '/name_servers=/s/^#//' "${ROOTFS_DIR}/etc/resolvconf.conf"
 
+# Remove the no-longer-needed line from /etc/hosts
+sed -i -e "/${TARGET_HOSTNAME}/d" "${ROOTFS_DIR}/etc/hosts"
