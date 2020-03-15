1c5fceda91236505475f5d0a161df851d06e1c38
Len Budney <Len.Budney@grantstreet.com>
2020-03-15 14:35:23 -0400

    Bug fix: kill gpg agent after importing a key, because the agent prevents the rootfs from unmounting.

--- 11-run-chroot.sh
+++ 11-run-chroot.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 # Install GPG public key for backup manager
 
+# Import the key, so backups can be encrypted.
 gpg --import /tmp/pubkey.gpg
+
+# Kill the GnuPG agent, which prevents the filesystem
+# from being unmounted later:
+ps aux |
+    egrep gpg-agen[t] | \
+    awk '{print $2}' | \
+    xargs kill -9
